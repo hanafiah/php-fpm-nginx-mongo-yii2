61425235949b97d07cd806e8cb437507d1ea8b0b
Hanafiah Yahya <ibnuyahya@gmail.com>
2017-08-22 01:16:18 +0800

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# php-fpm-nginx-mongo-yii2
+`docker-compose.yml` file and structure to setup php-fpm, nginx, mongo &amp; yii2 
+
+
+### Step  
+1. check your docker `preferences->file sharing`. make sure you've set your mount volume. Make sure to clone this repo under that particular volume. if not, you wont be able to create mount volume when run `docker-compose up`
+2. run `docker-compose up -d`
---
+++ web/stats-digital/common/config/main.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Hide index.php
+            'showScriptName' => false,
+            // Use pretty URLs
+            'enablePrettyUrl' => true,
+            'rules' => [
+            ],
+        ],
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://mongo:27017/statsdigital',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\MongoDbManager',
+            'defaultRoles' => ['guest'],
+        ],
+    ],
+    
+];
