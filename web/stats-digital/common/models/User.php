9d5b5943132439b13f1453dc824da66925ce71c6
Muhammad Fuad <m.fuad_90@yahoo.com>
2017-09-15 16:35:49 +0800

    new push from fuad

--- web/stats-digital/common/models/User.php
+++ web/stats-digital/common/models/User.php
@@ -4,7 +4,7 @@ namespace common\models;
 use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
-use yii\db\ActiveRecord;
+use yii\mongodb\ActiveRecord;
 use yii\web\IdentityInterface;
 
 /**
@@ -27,68 +27,69 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_ACTIVE = 10;
 
 
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
+    public static function collectionName()
     {
-        return '{{%user}}';
+        return ['statsdigital', 'user'];
     }
 
-    /**
-     * @inheritdoc
-     */
+
+    public function attributes()
+    {
+        return [
+            '_id',
+            'username',
+            'auth_key',
+            'password_hash',
+            'password_reset_token',
+            'email',
+            'status',
+            'created_at',
+            'updated_at',
+        ];
+    }
+
     public function behaviors()
     {
         return [
-            TimestampBehavior::className(),
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                   ActiveRecord::EVENT_BEFORE_INSERT => ['created_at','updated_at'],
+                   ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+            ],
         ];
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function rules()
     {
-        return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
-        ];
+      return [
+        ['status', 'default', 'value' => self::STATUS_ACTIVE],
+        ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+
+      ];
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public static function findIdentity($id)
     {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne($id);
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public static function findIdentityByAccessToken($token, $type = null)
     {
         throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
     }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
+
     public static function findByUsername($username)
     {
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
 
-    /**
-     * Finds user by password reset token
-     *
-     * @param string $token password reset token
-     * @return static|null
-     */
+
     public static function findByPasswordResetToken($token)
     {
         if (!static::isPasswordResetTokenValid($token)) {
@@ -101,12 +102,7 @@ class User extends ActiveRecord implements IdentityInterface
         ]);
     }
 
-    /**
-     * Finds out if password reset token is valid
-     *
-     * @param string $token password reset token
-     * @return bool
-     */
+
     public static function isPasswordResetTokenValid($token)
     {
         if (empty($token)) {
@@ -118,70 +114,49 @@ class User extends ActiveRecord implements IdentityInterface
         return $timestamp + $expire >= time();
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getId()
     {
         return $this->getPrimaryKey();
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function getAuthKey()
     {
         return $this->auth_key;
     }
 
-    /**
-     * @inheritdoc
-     */
+
     public function validateAuthKey($authKey)
     {
         return $this->getAuthKey() === $authKey;
     }
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
+
     public function validatePassword($password)
     {
         return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
 
-    /**
-     * Generates password hash from password and sets it to the model
-     *
-     * @param string $password
-     */
+
     public function setPassword($password)
     {
         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
     }
 
-    /**
-     * Generates "remember me" authentication key
-     */
+
     public function generateAuthKey()
     {
         $this->auth_key = Yii::$app->security->generateRandomString();
     }
 
-    /**
-     * Generates new password reset token
-     */
+
     public function generatePasswordResetToken()
     {
         $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
     }
 
-    /**
-     * Removes password reset token
-     */
+
     public function removePasswordResetToken()
     {
         $this->password_reset_token = null;
---
+++ web/stats-digital/vendor/yii2tech/embedded/CHANGELOG.md
@@ -0,0 +1,13 @@
+Yii 2 Embedded (Nested) Models extension Change Log
+===================================================
+
+1.0.1, October 17, 2016
+-----------------------
+
+- Enh #8: Added `Validator::initializedOnly` option allowing skip validation for not initialized embedded models (klimov-paul)
+
+
+1.0.0, December 26, 2015
+------------------------
+
+- Initial release.
